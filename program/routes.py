37f75e122df65fe1cf74a497ff40ab2722f4074c
gokul-elumalai <src.mv1f3@aleeas.com>
2023-10-28 16:59:46 +0530

    feat: Add Pokemon API with form

--- program/routes.py
+++ program/routes.py
@@ -1,8 +1,10 @@
 import requests
 
 from program import app
-from flask import render_template
+from flask import render_template, request
 
+
+VALID_COLOURS = ['red', 'blue', 'yellow', 'green', 'black']
 
 @app.route('/')
 @app.route('/index')
@@ -27,6 +29,17 @@ def advice():
     return render_template('advice.html', advice=sentence)
 
 
+@app.route('/pokemon', methods=['GET', 'POST'])
+def pokemon():
+    pokemon = []
+
+    if request.method == "POST" and 'pokecolour' in request.form:
+        colour = request.form.get('pokecolour')
+        if colour in VALID_COLOURS:
+            pokemon = get_poke_colours(colour)
+    return render_template('pokemon.html', pokemon=pokemon)
+
+
 def get_chuck_joke():
     r = requests.get('https://api.chucknorris.io/jokes/random')
     data = r.json()
@@ -37,3 +50,26 @@ def get_advice():
     r = requests.get('https://api.adviceslip.com/advice')
     data = r.json()
     return data['slip']['advice']
+
+
+def get_poke_colours(colour):
+    r = requests.get('https://pokeapi.co/api/v2/pokemon-color/' + colour.lower())
+    pokedata = r.json()
+    pokemons = []
+
+    for i in pokedata['pokemon_species'][:5]:
+        pokemon = {'name': i['name']}
+        r1 = requests.get(i['url'])
+        pokemon_stats = r1.json()
+        pokemon['habitat'] = pokemon_stats['habitat']['name']
+        pokemon['Legendary'] = pokemon_stats['is_legendary']
+
+        r2 = requests.get(pokemon_stats['evolution_chain']['url'])
+        evolution_stats = r2.json()['chain']['evolves_to']
+        species = [i['species']['name'] for i in evolution_stats]
+
+        pokemon['evolution'] = ' -> '.join(species)
+
+        pokemons.append(pokemon)
+
+    return pokemons
